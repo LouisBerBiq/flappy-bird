7d7b0f035059a00a4f502e4067a95c71501d602c
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-04-28 16:34:51 +0200

    Temporary changes

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,7 +1,7 @@
 const settings = {
 	canvas: document.querySelector('#game'),
-	canvasWidth: 288,
-	canvasHeight: 511,
+	canvasWidth: 288, // 288
+	canvasHeight: 511, // this.canvas.getAttribute("height")
 	canvasContext: null,
 	spriteSheetUrl: '../resources/sprite.png',
 
