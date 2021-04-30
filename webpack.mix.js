f9a6340c53a535ae1e2868f66f28314cf20a70c1
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-04-30 02:42:17 +0200

    Rebuilt package.json and removed typo in webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,6 @@ mix.disableNotifications();
 
 mix	.setPublicPath('dist/')
 	.copyDirectory('src/resources', 'dist/resources')
-	.copyDirectory('src/index.html', 'dist/')
+	.copy('src/index.html', 'dist/')
 	.js('src/js/main.js', 'dist/js/')
 	.sass('src/scss/main.scss', 'dist/css/')
