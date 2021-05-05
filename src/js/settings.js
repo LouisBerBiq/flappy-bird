3e50df6035e6f1efd23d97e50c969320bb104b9d
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-05-06 01:06:32 +0200

    tuned up some values

--- src/js/settings.js
+++ src/js/settings.js
@@ -8,13 +8,12 @@ const settings = {
 	spriteSheetUrl: '../resources/sprite.png',
 
 	scrollingSpeed: 3,
-	gravity: 1,
+	gravity: 0.5,
 	pipeSpacing: 280,
 	pipeSpacing2: 25,
 	init() {
 		this.canvasContext= this.canvas.getContext('2d');
 		this.sprite.src = this.spriteSheetUrl;
-		console.log(this.sprite);
 	}
 }
 
