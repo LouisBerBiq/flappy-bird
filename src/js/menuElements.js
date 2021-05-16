fc16009b817abd65bdab6cffd6dad717208e54ab
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-05-16 02:46:01 +0200

    made more menu elements available to JS

--- src/js/menuElements.js
+++ src/js/menuElements.js
@@ -1,5 +1,21 @@
 const menuElements = {
 	maintTitle : document.querySelector('#mainTitle'),
+	startButton : document.querySelector('#startButton'),
+	scoreButton : document.querySelector('#scoreButton'),
+	getReady : document.querySelector('#getReady'),
+	instructions : document.querySelector('#instructions'),
+	scoreCounterCountainer : document.querySelector('#scoreCounterCountainer'),
+	scoreCounterFirstDigit : document.querySelector('#scoreCounterFirstDigit'),
+	scoreCounterSecondDigit : document.querySelector('#scoreCounterSecondDigit'),
+	scoreCounterThirdDigit : document.querySelector('#scoreCounterThirdDigit'),
+	pause : document.querySelector('#pause'),
+	gameOver : document.querySelector('#gameOver'),
+	lifeRecap : document.querySelector('#lifeRecap'),
+	medal : document.querySelector('#medal'),
+	currentScore : document.querySelector('#currentScore'),
+	bestScore : document.querySelector('#bestScore'),
+	okContinue : document.querySelector('#okContinue'),
+	share : document.querySelector('#share'),
 }
 
 export default menuElements
