3809f9d4bb6061598cd39312cd41520e5f1548bc
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-05-24 18:11:26 +0200

    Added scoring sound

--- src/js/scoring.js
+++ src/js/scoring.js
@@ -2,16 +2,19 @@ import menuElements from "./menuElements";
 
 const scoring = {
 	score: 0,
+	audio: new Audio('../resources/pipe_pass.wav'),
 	scoreAnimationOffsetFirstDigit: 0,
 	scoreAnimationOffsetSecondDigit: 0,
 	scoreAnimationOffsetThirdDigit: 0,
 	firstPipe: true,
 	init(game) {
 		this.game = game
+		this.audio.volume = 0.3
 	},
 	scoreTimer() {
 		if (this.game.frameCounter >= this.game.frameInterval && !this.firstPipe) {
 			this.score++;
+			this.audio.play();
 
 			if (this.scoreAnimationOffsetSecondDigit < 0) {
 				menuElements.scoreCounterSecondDigit.style.display = 'block'
